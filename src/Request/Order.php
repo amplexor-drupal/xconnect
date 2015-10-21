1d6ad503c4e55b9ad96487ac000180f13f9b2d82
Peter Decuyper <sgrame@gmail.com>
2015-10-21 15:52:42 +0200

    Added the Order value object.

---
+++ src/Request/Order.php
@@ -0,0 +1,357 @@
+<?php
+
+namespace Amplexor\XConnect\Request;
+
+/**
+ * Class that represents the X-Connect order.
+ */
+class Order
+{
+    /**
+     * The order name.
+     *
+     * @var string
+     */
+    private $orderName;
+
+    /**
+     * The source language.
+     *
+     * @var string
+     */
+    private $sourceLanguage;
+
+    /**
+     * The target languages.
+     *
+     * @var array
+     */
+    private $targetLanguages = array();
+
+    /**
+     * The order instruction.
+     *
+     * @var array
+     */
+    private $instructions = array();
+
+    /**
+     * The client reference.
+     *
+     * @var string
+     */
+    private $reference;
+
+    /**
+     * The configuration.
+     *
+     * @var array
+     */
+    private $config = array(
+        'clientId'          => '',
+        'orderNamePrefix'   => '',
+        'templateId'        => '',
+        'dueDate'           => 0,
+        'issuedBy'          => '',
+        'isConfidential'    => false,
+        'service'           => '',
+        'needsConfirmation' => true,
+        'needsQuotation'    => false,
+    );
+
+    /**
+     * Array of files that are part of the order.
+     *
+     * @var array
+     */
+    private $files = array();
+
+
+    /**
+     * Create a new order.
+     *
+     * @param string $source_language
+     *   The source language for the translation order.
+     * @param array $config
+     *   The configuration elements for the order.
+     */
+    public function __construct($source_language, $config = array())
+    {
+        $this->sourceLanguage = $source_language;
+        $this->setConfig($config);
+
+        $this->orderName = $this->createOrderName();
+    }
+
+    /**
+     * Get the source language.
+     *
+     * @return string
+     *   The source language of the translation.
+     */
+    public function getSourceLanguage()
+    {
+        return $this->sourceLanguage;
+    }
+
+    /**
+     * Helper function to generate the order name when the object is created.
+     *
+     * @return string
+     */
+    protected function createOrderName()
+    {
+        $prefix = !empty($this->config['orderNamePrefix'])
+            ? $this->config['orderNamePrefix']
+            : 'translation_order';
+
+        // Add timestamp.
+        $timestamp = new \DateTime();
+
+        // Get a 3-digit microsecond representation.
+        $microseconds = floor((substr((string) microtime(), 1, 8) * 1000));
+        $microseconds = str_pad($microseconds, 3, '0', STR_PAD_LEFT);
+
+        return sprintf(
+            '%s_%s%s',
+            $prefix,
+            $timestamp->format('YmdHis'),
+            $microseconds
+        );
+    }
+
+    /**
+     * Fill in the configuration based on the given config array.
+     *
+     * @param array $config
+     *   The config array to store.
+     */
+    protected function setConfig($config)
+    {
+        $config_keys = array_keys($this->config);
+        foreach ($config_keys as $key) {
+            if (isset($config[$key])) {
+                $this->config[$key] = $config[$key];
+            }
+        }
+    }
+
+    /**
+     * Get the order name.
+     *
+     * @return string
+     *   The order name.
+     */
+    public function getOrderName()
+    {
+        return $this->orderName;
+    }
+
+    /**
+     * Get the client id.
+     *
+     * @return string
+     *   The client ID.
+     */
+    public function getClientId()
+    {
+        return $this->config['clientId'];
+    }
+
+    /**
+     * Get the template id.
+     *
+     * @todo: get data from where?
+     *
+     * @return string
+     *   The template ID.
+     */
+    public function getTemplateId()
+    {
+        return $this->config['templateId'];
+    }
+
+    /**
+     * Get the request date.
+     *
+     * @return DateTime
+     *   The date time presentation of the request date.
+     */
+    public function getRequestDate()
+    {
+        $date = new \DateTime();
+        return $date;
+    }
+
+    /**
+     * Get the request due date.
+     *
+     * The date is calculated based on the dueDate value in the settings.
+     *
+     * @return DateTime
+     *   The date time presentation of the due date.
+     */
+    public function getRequestDueDate()
+    {
+        // When no due date interval given, use today.
+        $dueDate = new \DateTime();
+        if (empty($this->config['dueDate'])) {
+            return $dueDate;
+        }
+
+        // Calculate the date by adding the dueDate interval.
+        $interval = new \DateInterval(
+            'P' . (int) $this->config['dueDate'] . 'D'
+        );
+        $dueDate->add($interval);
+
+        return $dueDate;
+    }
+
+    /**
+     * Get the issuer identifier.
+     *
+     * @return string
+     *   The email address.
+     */
+    public function getIssuedBy()
+    {
+        return $this->config['issuedBy'];
+    }
+
+    /**
+     * Get if the translation content is confidential.
+     *
+     * @return bool
+     *   Translation vcontent is confidential true/false.
+     */
+    public function isConfidential()
+    {
+        return (bool) $this->config['isConfidential'];
+    }
+
+    /**
+     * Add a target language.
+     *
+     * @param string $language
+     *   The target language to add to the order.
+     */
+    public function addTargetLanguage($language)
+    {
+        if (!in_array($language, $this->targetLanguages)) {
+            $this->targetLanguages[] = $language;
+        }
+    }
+
+    /**
+     * Get the target languages.
+     *
+     * @return array
+     *   The target language codes.
+     */
+    public function getTargetLanguages()
+    {
+        return $this->targetLanguages;
+    }
+
+    /**
+     * Get the service name.
+     *
+     * @return string
+     *   The Service name.
+     */
+    public function getService()
+    {
+        return $this->config['service'];
+    }
+
+    /**
+     * Add instruction to the order.
+     *
+     * @param string $instruction
+     *   The instruction.
+     */
+    public function addInstruction($instruction)
+    {
+        $this->instructions[] = $instruction;
+    }
+
+    /**
+     * Get the client instruction.
+     *
+     * @return string
+     *   The client instruction regarding the translation.
+     */
+    public function getInstructions()
+    {
+        return $this->instructions;
+    }
+
+    /**
+     * Set the client reference.
+     *
+     * @param string $reference
+     *   The reference to use in the order.
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+    }
+
+    /**
+     * Get the client reference.
+     *
+     * @return string
+     *   The client reference.
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * Is there a confirmation needed before the translation may be processed.
+     *
+     * @return bool
+     *   Needs confirmation true/false.
+     */
+    public function needsConfirmation()
+    {
+        return (bool) $this->config['needsConfirmation'];
+    }
+
+    /**
+     * Get if an quotation is required before translation is processed.
+     *
+     * @return bool
+     *   Needs quotation true/false.
+     */
+    public function needsQuotation()
+    {
+        return (bool) $this->config['needsQuotation'];
+    }
+
+    /**
+     * Add an input file to the order.
+     *
+     * @param string $file_name
+     *   The file name of the file to add to the order.
+     */
+    public function addFile($file_name)
+    {
+        if (!in_array($file_name, $this->files)) {
+            $this->files[] = $file_name;
+        }
+    }
+
+    /**
+     * Get the input files.
+     *
+     * @return array
+     *   The input file names part of the translation order.
+     */
+    public function getFiles()
+    {
+        return $this->files;
+    }
+}
---
+++ tests/Request/OrderTest.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace Amplexor\XConnect\Request\Test;
+
+use Amplexor\XConnect\Request\Order;
+
+class OrderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Source language.
+     */
+    public function testSourceLanguage()
+    {
+        $language1 = 'en';
+        $order1 = new Order($language1);
+
+        $this->assertEquals($language1, $order1->getSourceLanguage());
+
+        $language2 = 'nl';
+        $order2 = new Order($language2);
+        $this->assertEquals($language2, $order2->getSourceLanguage());
+    }
+
+    /**
+     * Order name.
+     */
+    public function testOrderName()
+    {
+        // Order name should be in the
+        // <default-prefix>_HHiiss<3-digit-microseconds> format.
+        $regex =
+            '/^%s_'
+            . date('Ymd')
+            . '(([0-1][0-9])|([2][0-3]))[0-5][0-9][0-9]{5}$/';
+
+        // Default prefixed with "translation_order".
+        $order1 = new Order('en');
+        $expectedNameRegExp1 = sprintf($regex, 'translation_order');
+        $this->assertRegExp($expectedNameRegExp1, $order1->getOrderName());
+
+        // Prefix from config array.
+        $prefix = 'my_order_test';
+        $order2 = new Order('en', array('orderNamePrefix' => $prefix));
+        $expectedNameRegExp2 = sprintf($regex, $prefix);
+        $this->assertRegExp($expectedNameRegExp2, $order2->getOrderName());
+    }
+
+    /**
+     * Client ID.
+     */
+    public function testClientId()
+    {
+        // Default empty.
+        $order1 = new Order('en');
+        $this->assertEquals('', $order1->getClientId());
+
+        // Client id from config array.
+        $clientId = '12345-abcdef-67890';
+        $order2 = new Order('en', array('clientId' => $clientId));
+        $this->assertEquals($clientId, $order2->getClientId());
+    }
+
+    /**
+     * Template ID.
+     */
+    public function testTemplateId()
+    {
+        // Default empty.
+        $order1 = new Order('en');
+        $this->assertEquals('', $order1->getTemplateId());
+
+        // Template id from config array.
+        $templateId2 = 'test-template-id';
+        $order2 = new Order('en', array('templateId' => $templateId2));
+        $this->assertEquals($templateId2, $order2->getTemplateId());
+    }
+
+    /**
+     * Request date.
+     */
+    public function testRequestDate()
+    {
+        $expectedDate1 = date('Ymd');
+
+        $order1 = new Order('en');
+        $this->assertInstanceOf('\DateTime', $order1->getRequestDate());
+        $this->assertEquals($expectedDate1, $order1->getRequestDate()->format('Ymd'));
+    }
+
+    /**
+     * Due date.
+     */
+    public function testDueDate()
+    {
+        // Default due date = 0 interval.
+        $expectedDate1 = new \DateTime();
+        $order1 = new Order('en');
+        $this->assertEquals(
+            $expectedDate1,
+            $order1->getRequestDueDate(),
+            'Request date should be now().',
+            // Allow 5 seconds interval because of computing cycles.
+            5
+        );
+
+        // Due date set using config.
+        $interval2 = new \DateInterval('P6D');
+        $expectedDate2 = new \DateTime();
+        $expectedDate2->add($interval2);
+        $order2 = new Order('en', array('dueDate' => 6));
+        $this->assertEquals(
+            $expectedDate2,
+            $order2->getRequestDueDate(),
+            'Request date should be now() + 6 days.',
+            // Allow 5 seconds interval because of computing cycles.
+            5
+        );
+    }
+
+    /**
+     * Issued By.
+     */
+    public function testIssuedBy()
+    {
+        // Default value.
+        $order1 = new Order('en');
+        $this->assertEquals('', $order1->getIssuedBy());
+
+        // Issued by from config array.
+        $issuedBy2 = 'test@my-domain.com';
+        $order2 = new Order('en', array('issuedBy' => $issuedBy2));
+        $this->assertEquals($issuedBy2, $order2->getIssuedBy());
+    }
+
+    /**
+     * Is confidential order.
+     */
+    public function testIsConfidential()
+    {
+        // Default value.
+        $order1 = new Order('en');
+        $this->assertFalse($order1->isConfidential());
+
+        // Is confidential from config array.
+        $order2 = new Order('en', array('isConfidential' => true));
+        $this->assertTrue($order2->isConfidential());
+    }
+
+    /**
+     * Target languages.
+     */
+    public function testTargetLanguages()
+    {
+        // Default none.
+        $order = new Order('en');
+        $this->assertEquals(array(), $order->getTargetLanguages());
+
+        // Add target languages.
+        $expectedLanguages = array('nl', 'fr');
+        $order->addTargetLanguage('nl');
+        $order->addTargetLanguage('fr');
+        $this->assertEquals($expectedLanguages, $order->getTargetLanguages());
+
+        // Add already added language.
+        $order->addTargetLanguage('nl');
+        $this->assertEquals($expectedLanguages, $order->getTargetLanguages());
+    }
+
+    /**
+     * Service.
+     */
+    public function testService()
+    {
+        // Default value.
+        $order1 = new Order('en');
+        $this->assertEquals('', $order1->getService());
+
+        // Service from config array.
+        $service = 'test-service';
+        $order2 = new Order('en', array('service' => $service));
+        $this->assertEquals($service, $order2->getService());
+    }
+
+    /**
+     * Test the instructions.
+     */
+    public function testInstructions()
+    {
+        // Default no instructions.
+        $order = new Order('en');
+        $this->assertEquals(array(), $order->getInstructions());
+
+        // Add some instructions.
+        $instructions = array(
+            'Instruction1 : Test of the instructions.',
+            'Instruction2 : Extra test for the instructions.'
+        );
+        $order->addInstruction($instructions[0]);
+        $order->addInstruction($instructions[1]);
+        $this->assertEquals($instructions, $order->getInstructions());
+    }
+
+    /**
+     * Test the reference.
+     */
+    public function testReference()
+    {
+        // Default no value.
+        $order = new Order('en');
+        $this->assertEquals('', $order->getReference());
+
+        // Add a reference.
+        $reference = 'test-reference';
+        $order->setReference($reference);
+        $this->assertEquals($reference, $order->getReference());
+    }
+
+    /**
+     * Test needs confirmation.
+     */
+    public function testNeedsConfirmation()
+    {
+        // Default value = true.
+        $order1 = new Order('en');
+        $this->assertTrue($order1->needsConfirmation());
+
+        // Is confidential from config array.
+        $order2 = new Order('en', array('needsConfirmation' => false));
+        $this->assertFalse($order2->needsConfirmation());
+    }
+
+    /**
+     * Test needs quotation.
+     */
+    public function testNeedsQuotation()
+    {
+        // Default value = true.
+        $order1 = new Order('en');
+        $this->assertFalse($order1->needsQuotation());
+
+        // Is confidential from config array.
+        $order2 = new Order('en', array('needsQuotation' => true));
+        $this->assertTrue($order2->needsQuotation());
+    }
+
+    /**
+     * Test the files.
+     */
+    public function testFiles()
+    {
+        // Default no files.
+        $order = new Order('en');
+        $this->assertEquals(array(), $order->getFiles());
+
+        // Add some files.
+        $files = array(
+            'test-file-1.html',
+            'test-file-2.html'
+        );
+        $order->addFile($files[0]);
+        $order->addFile($files[1]);
+        $this->assertEquals($files, $order->getFiles());
+    }
+}
