960b6c77db57014debb69b6fd32d539dc7c65c1d
Peter Decuyper <sgrame@gmail.com>
2015-10-23 16:09:35 +0200

    Added missing docheads.

--- src/Request.php
+++ src/Request.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the Amplexor\XConnect library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/amplexor-drupal/xconnect/
+ * @version 1.0.0
+ * @package Amplexor.XConnect
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Amplexor\XConnect;
 
