b6cc6895bec9298eca07064ba71485619d4f1a8c
Peter Decuyper <sgrame@gmail.com>
2015-10-27 16:40:09 +0100

    Added translations iterator.

---
+++ src/Response/Translations.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * This file is part of the Amplexor\XConnect library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/amplexor-drupal/xconnect/
+ * @version 1.0.0
+ * @package Amplexor.XConnect
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Amplexor\XConnect\Response;
+
+use Amplexor\XConnect\Response\File\FileInterface;
+use Amplexor\XConnect\Response\Translation;
+
+/**
+ * Class representing a collection of translations.
+ */
+class Translations implements \Iterator, \Countable
+{
+    /**
+     * The collection.
+     *
+     * @var Translation[]
+     */
+    private $translations = [];
+
+    /**
+     * The current item pointer.
+     *
+     * @var int
+     */
+    private $pointer = 0;
+
+
+    /**
+     * Construct the translations collection from the info file.
+     *
+     * @param FileInterface $file
+     *   The Response file object.
+     * @param InfoFiles $infoFiles
+     *   The info files collection.
+     */
+    public function __construct(FileInterface $file, InfoFiles $infoFiles)
+    {
+        while ($infoFiles->valid()) {
+            $this->translations[] = new Translation($file, $infoFiles->current());
+            $infoFiles->next();
+        }
+    }
+
+    /**
+     * Get the current translation.
+     */
+    public function current()
+    {
+        if (!$this->valid()) {
+            return;
+        }
+        return $this->translations[$this->pointer];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function next()
+    {
+        $this->pointer++;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function key()
+    {
+        return $this->pointer;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function valid()
+    {
+        return array_key_exists($this->pointer, $this->translations);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rewind()
+    {
+        $this->pointer = 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function count()
+    {
+        return count($this->translations);
+    }
+}
---
+++ tests/Response/TranslationsTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Amplexor\XConnect\Response\Translations\Test;
+
+use Amplexor\XConnect\Response\Translation;
+use Amplexor\XConnect\Response\Translations;
+
+class TranslationsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Create a InfoFile mock.
+     *
+     * @param string $fileName
+     *   The file name to use in the mock object.
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getInfoFileMock($fileName = 'translation.html')
+    {
+        $infoFile = $this
+            ->getMockBuilder('Amplexor\XConnect\Response\InfoFile')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $infoFile->method('getName')
+            ->willReturn($fileName);
+
+        return $infoFile;
+    }
+
+    /**
+     * Create a InfoFiles mock.
+     *
+     * @param int $amount
+     *   The number of files in the mock.
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getInfoFilesIteratorMock($amount = 0)
+    {
+        $infoFiles = $this
+            ->getMockBuilder('Amplexor\XConnect\Response\InfoFiles')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Add the requested number of mock files to collection.
+        // PhpUnitMock has in internal counter (at) for whatever method call on
+        // the object. We need to set the expected counter when creating the
+        /// mock.
+        $j = 0;
+        for ($i = 0; $i < $amount; $i++) {
+            $infoFiles->expects($this->at($j))
+                ->method('valid')
+                ->willReturn(true);
+            $j++;
+            $infoFiles->expects($this->at($j))
+                ->method('current')
+                ->willReturn(
+                    $this->getInfoFileMock('file-' . $i . '.html')
+                );
+            $j++;
+            // One extra since we are calling the next() method.
+            $j++;
+        }
+
+        $infoFiles->expects($this->at($j))
+            ->method('valid')
+            ->willReturn(false);
+
+        return $infoFiles;
+    }
+
+    /**
+     * Create a FileInterface mock.
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getFileMock()
+    {
+        $file = $this
+            ->getMockBuilder('Amplexor\XConnect\Response\File\FileInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $file;
+    }
+
+
+    /**
+     * Test countable.
+     */
+    public function testCountable()
+    {
+        // No translations.
+        $translations = new Translations(
+            $this->getFileMock(),
+            $this->getInfoFilesIteratorMock(0)
+        );
+        $this->assertEquals(0, $translations->count());
+
+        // 4 translations.
+        $translations = new Translations(
+            $this->getFileMock(),
+            $this->getInfoFilesIteratorMock(4)
+        );
+        $this->assertEquals(4, count($translations));
+    }
+
+    /**
+     * Test traversable.
+     */
+    public function testTraversable()
+    {
+        $translations = new Translations(
+            $this->getFileMock(),
+            $this->getInfoFilesIteratorMock(5)
+        );
+
+        $expected = [
+            'file-0.html',
+            'file-1.html',
+            'file-2.html',
+            'file-3.html',
+            'file-4.html',
+        ];
+
+        foreach ($translations as $i => $translation) {
+            $this->assertInstanceOf(
+                'Amplexor\XConnect\Response\Translation',
+                $translation
+            );
+            $this->assertEquals(
+                $expected[$i],
+                $translation->getInfo()->getName()
+            );
+        }
+    }
+
+    /**
+     * Test current()
+     */
+    public function testCurrent()
+    {
+        $translations = new Translations(
+            $this->getFileMock(),
+            $this->getInfoFilesIteratorMock(0)
+        );
+
+        $this->assertNull($translations->current());
+    }
+}
